2cc31898dfa5bcaa0ff24f8571595f64663b6fc1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-02 22:18:26 +0000

    Deploy music-markdown/music-markdown.github.io to github.com/music-markdown/music-markdown.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60665b4b32b25284d65a6d555b133f9c.js"
+  "/precache-manifest.d7cdb98ad5cce8d232347334f5ca64af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
